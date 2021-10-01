1e3cec075e061abfd82a4b21111f09a70ca30924
sm66589 <SieteOcho78&*>
2021-10-01 13:13:52 -0500

    added comments

--- simpleCounter.py
+++ simpleCounter.py
@@ -3,15 +3,33 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define task and input channel
+'''
+Simple counter of rising edges. Based on 
+https://github.com/ni/nidaqmx-python/blob/master/nidaqmx_examples/ci_count_edges.py
+
+As of 10/01/2021, with input TTL pulses at 200kHz, the counter works using the code below. I don't understand how. 
+'''
+
 device = "Dev4"
 
 task = ni.Task()
-task.ci_channels.add_ci_count_edges_chan(device + "/ctr0")
-task.start()
-time.sleep(1)
-task.stop()
+task.ci_channels.add_ci_count_edges_chan(device + "/ctr0")     # Denotes the channel that will make the counting
+task.ci_channels[0].ci_count_edges_term = "/Dev4/PFI2"         # Denotes the source of the edges to count
 
-print(task.read())
+#                                                              # From the test panel of NI MAX, it can be seen that the
+#                                                              counting channel is not the same as the edge source. Not
+#                                                              sure why.
+
+
+countInterval = 10 #seconds
+
+task.start()
+initialTime = time.time()
+while time.time() - initialTime <= countInterval:
+    data = task.read()
+
+print(data)
 
 task.close()
+#TODO: Understand how task.read() works for counters. Why does data need to be redefines everytime during the loop?
+
